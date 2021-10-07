6dad0cfba8c7b7282c900cdb3802ca1910c9281c
DerpfaceMarcell <agnesmarcella@rocketmail.com>
2021-10-07 08:29:49 +0700

    Add DOM - Mouseleave's still not working

---
+++ public/js/homepage.js
@@ -0,0 +1,21 @@
+// Section 2
+sec2Cards = document.querySelectorAll(".sec2-card");
+
+const setOrangeCard = (cardElement) => {
+    cardElement.style.backgroundColor = "orange";
+    cardElement.style.color = "white";
+}
+
+const removeOrangeCard = (cardElement) => {
+    cardElement.style.setBackgroundColor = "white";
+    cardElement.style.color = "black";
+}
+
+for (let i = 0; i < sec2Cards.length; i++) {
+    sec2Cards[i].addEventListener("mouseover", function () {
+        setOrangeCard(sec2Cards[i]);
+    });
+    sec2Cards[i].addEventListener("mouseleave", function () {
+        removeOrangeCard(sec2Cards[i]);
+    });
+}
